07b7c531848f907af875982d2373db3fb9bfb344
akshitaadhikari <136777778+akshitaadhikari@users.noreply.github.com>
2023-12-21 21:55:45 +0530

    Create _3_demo.scala

---
+++ _3_demo.scala
@@ -0,0 +1,13 @@
+package Demo
+
+object _3_demo {
+  def main(args: Array[String]): Unit = {
+    val x :Int = 10
+    val var1 : String = "Apple"
+    val y : Float = 34.7f
+    val( a: Int , b : String) = (12 , "hello")
+
+    println(x) ; println(var1) ; println(y) ; println(a) ; println(b)
+  }
+
+}
