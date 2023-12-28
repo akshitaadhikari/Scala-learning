a68b797b278f5769bbcfdb1b27845d83150192cb
akshitaadhikari <136777778+akshitaadhikari@users.noreply.github.com>
2023-12-28 18:52:04 +0530

    Create _9_Arrays.scala

---
+++ _9_Arrays.scala
@@ -0,0 +1,10 @@
+package Demo
+
+object _9_Arrays {
+  def main(args : Array[String]) : Unit = {
+    val numbers : Array[Int] = Array( 2 , 4, 23, 45, 34)
+    val words : Array[String] = Array( "cat" , "dog" , "elephant")
+    val a : Int = numbers(1)
+    println(a)
+  }
+}
