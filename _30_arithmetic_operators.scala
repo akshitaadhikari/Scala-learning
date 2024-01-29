adbe16b392092a7cfed8f32617ab11cfac314374
akshitaadhikari <136777778+akshitaadhikari@users.noreply.github.com>
2024-01-29 15:46:35 +0530

    Create _30_arithmetic_operators.scala

---
+++ _30_arithmetic_operators.scala
@@ -0,0 +1,19 @@
+package Demo
+
+object _30_arithmetic_operators extends App{
+  var a: Int = 4
+  val b:Int = 5
+
+  val c :Int = a+b
+  println(c)
+  val d :Int = a-b
+  println(d)
+  val e :Int = a*b
+  println(e)
+  val f :Int = a/b
+  println(f)
+  val g: Int = a % b
+  println(g)
+  val h: Int = a+7
+  println(h)
+}
